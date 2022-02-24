1b46d34beafb156b7874f6c99d73ec884d7c3b4d
Cameron Waterman <cameron.waterman@ni.com>
2022-02-24 11:43:22 -0600

    Add iframeNavigate postMessage calls

--- public/app/core/navigation/GrafanaRoute.tsx
+++ public/app/core/navigation/GrafanaRoute.tsx
@@ -26,6 +26,8 @@ export function GrafanaRoute(props: Props) {
     updateBodyClassNames(props.route);
     cleanupDOM();
     navigationLogger('GrafanaRoute', false, 'Mounted', props.match);
+    // TODO: Wrap this in a conditional that checks the config value "allow_embedding"
+    window.parent.postMessage('iframeNavigate', window.parent.location.origin);
 
     return () => {
       navigationLogger('GrafanaRoute', false, 'Unmounted', props.route);
@@ -39,6 +41,8 @@ export function GrafanaRoute(props: Props) {
     cleanupDOM();
     reportPageview();
     navigationLogger('GrafanaRoute', false, 'Updated', props);
+    // TODO: Wrap this in a conditional that checks the config value "allow_embedding"
+    window.parent.postMessage('iframeNavigate', window.parent.location.origin);
   });
 
   navigationLogger('GrafanaRoute', false, 'Rendered', props.route);
