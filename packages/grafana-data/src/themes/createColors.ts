ea8726e323a820ed4b46c373269e05caf894db18
Cameron Waterman <cameron.waterman@ni.com>
2022-03-16 11:49:37 -0500

    NI Green

--- packages/grafana-data/src/themes/createColors.ts
+++ packages/grafana-data/src/themes/createColors.ts
@@ -229,8 +229,8 @@ class LightColors implements ThemeColorsBase<Partial<ThemeRichColor>> {
   };
 
   gradients = {
-    brandHorizontal: 'linear-gradient(90deg, #FF8833 0%, #F53E4C 100%);',
-    brandVertical: 'linear-gradient(0.01deg, #F53E4C -31.2%, #FF8833 113.07%);',
+    brandHorizontal: 'linear-gradient(90deg, #26A97C 0%, #009B65 100%);',
+    brandVertical: 'linear-gradient(0.01deg, #009B65 -31.2%, #26A97C 113.07%);',
   };
 
   contrastThreshold = 3;
