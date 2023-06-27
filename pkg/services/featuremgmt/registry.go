174956240e6f984613120c29aec96e87db569592
mure <carsonatxmoore@gmail.com>
2023-06-27 17:14:15 -0500

    Disable connections page and new navigation

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -213,7 +213,7 @@ var (
 			Name:        "dataConnectionsConsole",
 			Description: "Enables a new top-level page called Connections. This page is an experiment that provides a better experience when you install and configure data sources and other plugins.",
 			State:       FeatureStateStable,
-			Expression:  "true", // turned on by default
+			Expression:  "false", // turned on by default
 			Owner:       grafanaPluginsPlatformSquad,
 		},
 		{
@@ -227,7 +227,7 @@ var (
 			Name:        "topnav",
 			Description: "Enables new top navigation and page layouts",
 			State:       FeatureStateStable,
-			Expression:  "true", // enabled by default
+			Expression:  "false", // enabled by default
 			Owner:       grafanaUserEssentialsSquad,
 		},
 		{
